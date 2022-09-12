c93e683d88b0c3768a109379158c894dae9934f4
Lucy de Rojas <lucyderojas@gmail.com>
2022-09-12 21:56:43 +0100

    added sitemap and robots

---
+++ interActive.js
@@ -0,0 +1,7 @@
+let homeButton = document.getElementsByClassName('buttonHome');
+
+let secondButton = document.getElementById('buttonSecond');
+
+secondButton.onmouseover(function()=>{
+    secondButton.style.color='blue';
+});
